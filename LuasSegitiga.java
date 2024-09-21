4d2f91fe69d0a75c45b3679beec2b2aee2689ddf
RadityaHernandaAkbar <hernandaakbarraditya@gmail.com>
2024-09-21 18:23:14 +0800

    Add files via upload

---
+++ LuasSegitiga.java
@@ -0,0 +1,13 @@
+package tipedata;
+
+public class LuasSegitiga {
+
+    public static void main(String[] args) {
+        int alas = 7;
+        int tinggi = 3;
+        double setengah = 0.5;
+        double luas = setengah * alas * tinggi;
+
+        System.out.println("luas segitiga : " + luas);
+    }
+}
